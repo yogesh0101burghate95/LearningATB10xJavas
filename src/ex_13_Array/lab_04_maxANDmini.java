417f049e62205b2a222de035434f9a6d77ffa7e7
Yogeh <yogeshburghate01@gmail.com>
2025-06-11 11:20:43 +0530

    Merge remote-tracking branch 'origin/master'

--- src/ex_13_Array/lab_04_maxANDmini.java
+++ src/ex_13_Array/lab_04_maxANDmini.java
@@ -2,12 +2,34 @@ package ex_13_Array;
 
 public class lab_04_maxANDmini {
     public static void main(String[] args) {
-        int[] marks = {65,54,78,86,59,83,95,79,67,85};
+        int[] marks = {65, 54, 78, 86, 59, 83, 95, 79, 67, 85};
+
+        int max_output = give_max(marks);
+        System.out.println("The maximum number: "+max_output);
+        int mini_output = give_mini(marks);
+        System.out.println("The maximum number: "+mini_output);
+
+    }
+       static int give_max(int[] marks){
         int max = marks[0];
 
         for (int i=0; i<marks.length; i++){
-            max = marks[i];
-
+            if (marks[i] > max){
+                max = marks[i];
+            }
         }
+           return max;
+       }
+
+    static int give_mini(int[] marks){
+        int mini = marks[0];
+
+        for (int i=0; i<marks.length; i++){
+            if (marks[i] < mini){
+                mini = marks[i];
+            }
+        }
+        return mini;
     }
-}
+    }
+
