3d2beede3466e499f73406aaa36585aace2eac8e
Yogeh <yogeshburghate01@gmail.com>
2025-06-23 15:41:59 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ src/ex_14_Oops/lab_01_basics.java
@@ -0,0 +1,16 @@
+package ex_14_Oops;
+
+public class lab_01_basics {
+    public static void main(String[] args) {
+
+        Vahicle v1 = new Vahicle();
+        v1.car();
+    }
+}
+     class Vahicle {
+        void car() {
+            System.out.println("Harrier");
+        }
+
+    }
+
